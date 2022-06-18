0247da05c41826672f07c21957550e58f7cdcc3e
th3g3ntl3boy <afick98@gmail.com>
2022-06-18 08:42:37 +0800

    typwrite effect

--- src/Komponen/Header.js
+++ src/Komponen/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import Typewriter from 'typewriter-effect'
 
 const Header = () =>{
     return(
         <div>
             <div className="text-center">
                 <h1>Welcome to Auto Algorithm</h1>
-                <p><i>"a simple way to understand the data"</i></p>
+                <p><i>
+                    <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter.typeString('"a simple way to understand the data"').start()
+                    }}
+                    />
+                </i></p>
                 <hr></hr>
             </div>
         </div>
